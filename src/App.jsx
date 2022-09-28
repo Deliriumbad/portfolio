d2640943b33199a77f87025f6b34c03bf11c7cbe
Vit <vit_waladzko@mail.ru>
2022-09-28 16:54:10 +0300

    part 2/3 variables, mixins

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Header} from "./components/Header/Header";
-import './App.css';
+import './App.scss';
 import {Main} from "./components/Main/Main";
 import {Scills} from "./components/Skills/Scills";
 import {Portfolio} from './components/Portfolio/Portfolio';
